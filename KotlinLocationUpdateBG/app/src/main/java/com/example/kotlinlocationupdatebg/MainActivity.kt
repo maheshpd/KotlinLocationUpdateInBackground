f4b7960fc9d5a92e804af9a2d38d6bb12786f3b3
Sangram <kirtirsonawane@gmail.com>
2021-03-22 10:04:51 +0530

    Update in map

--- KotlinLocationUpdateBG/app/src/main/java/com/example/kotlinlocationupdatebg/MainActivity.kt
+++ KotlinLocationUpdateBG/app/src/main/java/com/example/kotlinlocationupdatebg/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.example.kotlinlocationupdatebg
 
 import android.Manifest
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.widget.RemoteViews
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -18,11 +26,19 @@ import com.karumi.dexter.listener.PermissionGrantedResponse
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.single.PermissionListener
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var locationRequest: LocationRequest
     lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    // declaring variables
+    lateinit var notificationManager: NotificationManager
+    lateinit var notificationChannel: NotificationChannel
+    lateinit var builder: Notification.Builder
+    private val channelId = "i.apps.notifications"
+    private val description = "Test notification"
 
     companion object {
         var instance: MainActivity? = null
@@ -38,15 +54,42 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    fun createNotification() {
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        val contentView = RemoteViews("com.example.kotlinlocationupdatebg", R.layout.activity_after_notification)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationChannel = NotificationChannel(channelId, description, NotificationManager.IMPORTANCE_HIGH)
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.GREEN
+            notificationChannel.enableVibration(false)
+            notificationManager.createNotificationChannel(notificationChannel)
+
+            builder = Notification.Builder(this, channelId)
+                    .setContent(contentView)
+                    .setSmallIcon(R.drawable.ic_launcher_background)
+                    .setLargeIcon(BitmapFactory.decodeResource(this.resources, R.drawable.ic_launcher_background))
+                    .setContentIntent(pendingIntent)
+        } else {
+            builder = Notification.Builder(this)
+                    .setContent(contentView)
+                    .setSmallIcon(R.drawable.ic_launcher_background)
+                    .setLargeIcon(BitmapFactory.decodeResource(this.resources, R.drawable.ic_launcher_background))
+                    .setContentIntent(pendingIntent)
+        }
+        notificationManager.notify(1234, builder.build())
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        Done
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         instance = this
         Dexter.withContext(this)
-            .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
             .withListener(object : PermissionListener {
                 override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
                     updateLocation()
@@ -97,7 +140,5 @@ class MainActivity : AppCompatActivity() {
         locationRequest.interval = 5000
         locationRequest.fastestInterval = 3000
         locationRequest.smallestDisplacement = 10f
-
-
     }
 }
--- KotlinLocationUpdateBG/app/src/main/java/com/example/kotlinlocationupdatebg/MyLocationService.kt
+++ KotlinLocationUpdateBG/app/src/main/java/com/example/kotlinlocationupdatebg/MyLocationService.kt
@@ -1,8 +1,15 @@
 package com.example.kotlinlocationupdatebg
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
+import android.widget.RemoteViews
 import android.widget.Toast
 import com.google.android.gms.location.LocationResult
 
@@ -12,25 +19,97 @@ class MyLocationService : BroadcastReceiver() {
         val ACTION_PROCESS_UPDATE = "com.example.kotlinlocationupdatebg.UPDATE_LOCATION"
     }
 
+    // declaring variables
+    lateinit var notificationManager: NotificationManager
+    lateinit var notificationChannel: NotificationChannel
+    lateinit var builder: Notification.Builder
+    private val channelId = "i.apps.notifications"
+    private val description = "Test notification"
+
+
     override fun onReceive(context: Context?, intent: Intent?) {
+        notificationManager = context!!.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         if (intent != null) {
             val action = intent.action
             if (action.equals(ACTION_PROCESS_UPDATE)) {
                 val result = LocationResult.extractResult(intent)
                 if (result != null) {
                     val location = result.lastLocation
+                    val latitude: Double = location.latitude
+                    val longitude: Double = location.longitude
+
                     val location_string = StringBuilder(location.latitude.toString())
-                        .append("/").append(location.longitude).toString()
+                            .append("/").append(location.longitude).toString()
+
                     try {
                         MainActivity.getMainInstance().updateTextView(location_string)
                     } catch (e: Exception) {
                         //if app in killes mode
                         Toast.makeText(context, location_string, Toast.LENGTH_SHORT).show()
+                        createNotification(context, latitude, longitude)
                     }
                 }
             }
         }
     }
+
+    private fun createNotification(context: Context, latitude: Double, longitude: Double) {
+        val intent = Intent(context, MapsActivity::class.java)
+        intent.putExtra("latitude", latitude)
+        intent.putExtra("longitude", longitude)
+
+        val pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        val contentView = RemoteViews("com.example.kotlinlocationupdatebg", R.layout.activity_after_notification)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationChannel = NotificationChannel(channelId, description, NotificationManager.IMPORTANCE_HIGH)
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.GREEN
+            notificationChannel.enableVibration(true)
+            notificationManager.createNotificationChannel(notificationChannel)
+
+            builder = Notification.Builder(context, channelId)
+                    .setContent(contentView)
+                    .setSmallIcon(R.drawable.ic_launcher_background)
+                    .setContentIntent(pendingIntent)
+        } else {
+            builder = Notification.Builder(context)
+                    .setContent(contentView
+                    ).setVibrate(listOf(1L, 2L, 3L).toLongArray())
+                    .setSmallIcon(R.drawable.ic_launcher_background)
+                    .setContentIntent(pendingIntent)
+        }
+        notificationManager.notify(1234, builder.build())
+    }
+
+/*
+    fun createNotification(context: Context?){
+        val intent = Intent(context, MapsActivity::class.java)
+
+        val pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        val contentView = RemoteViews("com.example.kotlinlocationupdatebg",R.layout.activity_after_notification)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationChannel = NotificationChannel(channelId,description,NotificationManager.IMPORTANCE_HIGH)
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.GREEN
+            notificationChannel.enableVibration(true)
+            notificationManager.createNotificationChannel(notificationChannel)
+
+            builder = Notification.Builder(context, channelId)
+                    .setContent(contentView)
+                    .setSmallIcon(R.drawable.ic_launcher_background)
+                    .setContentIntent(pendingIntent)
+        } else {
+            builder = Notification.Builder(context)
+                    .setContent(contentView
+                    ).setVibrate(listOf(1L, 2L, 3L).toLongArray())
+                    .setSmallIcon(R.drawable.ic_launcher_background)
+                    .setContentIntent(pendingIntent)
+        }
+        notificationManager.notify(1234, builder.build())
+    }
+*/
 
 
 }
